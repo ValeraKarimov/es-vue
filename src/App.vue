509d8c031a568eb60e065db0e0d9b6f945e9e49b
Valerii Karimov <valeraguess@gmail.com>
2021-09-21 15:23:13 +0200

    upload via files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,94 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <h1> {{ firstName }}</h1>
+    <h2> {{ num*2 }}</h2>
+    <h3> {{ num > 1 ? 'Num > 1' : 'Num smaller less 1' }}</h3>
+    <h3> {{ obj.age}}</h3>
+    <a :href="linkUrl">Link to google</a>
+
+    <template v-if="msgIsVisible === 1"><div class="message">Some message!</div></template>
+    <template v-else-if="msgIsVisible === 2"><div class="message">second message!</div></template>
+    <template v-else><div class="message">Another message!</div></template>
+
+    <div v-show="isVisibleBlock">V show example</div>
+
+    <div class="name-input" v-if="inputNameVisible">
+      <label>Name</label>
+      <input type="text" placeholder="input name" key="name-input">
+    </div>
+
+    <div class="nickname-input" v-else>
+      <label>NickName</label>
+      <input type="text" placeholder="input nickname" key="nickname-input">
+    </div>
+
+<button @click="inputNameVisible = !inputNameVisible">Change Form</button>
+
+<button @click.right="(onClik('some value', $event))">Click event</button>
+
+<a href="" @click.prevent="onLinkClick">Link</a>
+
+<h1>{{ text }}</h1>
+
+<input type="text" @keyup.enter="onKeyUp" />
+
+<ul>
+  <li v-for="(color, index) in colors" :key="index"> {{ index + 1}} {{ color }}</li>
+  <li v-for="user in users" :key="user.id">{{ user.name }} : {{ user.age }}</li>
+  <li v-for="(value, name, index) in product" :key="index" >
+    {{ index }}) {{ name }} : {{ value }}</li>
+    <div>
+      <input type="text" @keyup.enter="addNewColor">
+      <input type="text" @keyup.enter="deleteProp">
+    </div>
+</ul>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+  },
+  data: () => ({
+    text: '',
+    firstName: 'Valerii',
+    num: 2,
+    obj: {
+      age: 30,
+    },
+    linkUrl: 'https://google.com',
+    msgIsVisible: 1,
+    isVisibleBlock: true,
+    inputNameVisible: true,
+    colors: ['orange', 'red', 'blue', 'black'],
+    users: [{ name: 'Valerii', age: '26', id: '1' }, { name: 'Denis', age: '30', id: '2' }],
+    product: {
+      brand: 'Apple',
+      model: 'Iphone 11 Pro',
+      price: '$1000',
+    },
+  }),
+  methods: {
+    onClik(value, e) {
+      console.log(value, e);
+    },
+    onLinkClick() {
+      console.log('Link Click');
+    },
+    onKeyUp(e) {
+      console.log(e);
+      this.text = e.target.value;
+    },
+    addNewColor(e) {
+      this.colors.push(e.target.value);
+      console.log(this);
+    },
+    deleteProp(e) {
+      this.delete.product(this.product, e.target.value);
+      console.log(this.product);
+    },
   },
 };
 </script>
